6ca4637d6bcfa28c19539ab11de7ca4e9152b7a4
Estelle Comment <estelle.comment@gmail.com>
2026-07-02 16:06:32 +0200

    Tests for BlogIndexPage, mirroring the ones for PublicationIndexPage

---
+++ sites_conformes/blog/tests/test_blog_index_page.py
@@ -0,0 +1,248 @@
+import zoneinfo
+from datetime import datetime
+from itertools import combinations
+
+from bs4 import BeautifulSoup
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext
+from wagtail.models import Page
+from wagtail.test.utils import WagtailPageTestCase
+
+from sites_conformes.blog.models import BlogEntryPage, BlogIndexPage, Category, Organization, Person
+from sites_conformes.core.models import Tag
+
+User = get_user_model()
+
+# BlogIndexPage filter toggles: category and tag default on; author and source default off.
+FILTER_SETTINGS_DEFAULTS = {
+    "filter_by_category": True,
+    "filter_by_tag": True,
+    "filter_by_author": False,
+    "filter_by_source": False,
+}
+
+FILTER_CASES = [
+    {
+        "name": "category",
+        "setting": "filter_by_category",
+        "heading": gettext("Filter by category"),
+        "visible_label": lambda self: self.category.name,
+        "query_param": lambda self: "category=agriculture",
+        "post_kwargs": lambda self: {"blog_categories": [self.category]},
+        "matching_title": lambda self: self.post_with_category.title,
+        "other_title": lambda self: self.post_with_other_category.title,
+    },
+    {
+        "name": "tag",
+        "setting": "filter_by_tag",
+        "heading": gettext("Filter by tag"),
+        "visible_label": lambda self: self.tag.name,
+        "query_param": lambda self: "tag=news",
+        "post_kwargs": lambda self: {"tags": [self.tag]},
+        "matching_title": lambda self: self.post_with_tag.title,
+        "other_title": lambda self: self.post_with_other_tag.title,
+    },
+    {
+        "name": "author",
+        "setting": "filter_by_author",
+        "heading": gettext("Filter by author"),
+        "visible_label": lambda self: self.author.name,
+        "query_param": lambda self: f"author={self.author.id}",
+        "post_kwargs": lambda self: {"authors": [self.author]},
+        "matching_title": lambda self: self.post_with_author.title,
+        "other_title": lambda self: self.post_with_other_author.title,
+    },
+    {
+        "name": "source",
+        "setting": "filter_by_source",
+        "heading": gettext("Filter by source"),
+        "visible_label": lambda self: self.organization.name,
+        "query_param": lambda self: "source=inrae",
+        # You can't assign a source to a post directly, so we assign an author associated to the source.
+        "post_kwargs": lambda self: {"authors": [self.author]},
+        "matching_title": lambda self: self.post_with_author.title,
+        "other_title": lambda self: self.post_with_other_author.title,
+    },
+]
+
+for case in FILTER_CASES:
+    case["filter_url"] = lambda self, case=case: (f"{self.index.url}?{case['query_param'](self)}")
+
+
+def list_settings_in_panel(panels):
+    names = []
+    for panel in panels:
+        if hasattr(panel, "field_name"):
+            names.append(panel.field_name)
+        if hasattr(panel, "children"):
+            names.extend(list_settings_in_panel(panel.children))
+    return names
+
+
+class BlogIndexPageSettingsTest(WagtailPageTestCase):
+    def test_settings_show_filters_panel_includes_all_fields(self):
+        field_names = list_settings_in_panel(BlogIndexPage.settings_panels)
+        for field_name in FILTER_SETTINGS_DEFAULTS:
+            self.assertIn(field_name, field_names)
+
+    def test_filter_settings_default_values(self):
+        home = Page.objects.get(slug="home")
+        page = home.add_child(
+            instance=BlogIndexPage(title="Defaults", slug="defaults"),
+        )
+        for field_name, expected_default in FILTER_SETTINGS_DEFAULTS.items():
+            self.assertEqual(getattr(page, field_name), expected_default, field_name)
+
+
+class BlogIndexPageFilterTestBase(WagtailPageTestCase):
+    def setUp(self):
+        self.home = Page.objects.get(slug="home")
+        self.admin = User.objects.create_superuser("test", "test@test.test", "pass")
+        self.admin.save()
+        self.paris_tz = zoneinfo.ZoneInfo("Europe/Paris")
+
+        self.index = self.home.add_child(
+            instance=BlogIndexPage(
+                title="Blog",
+                slug="blog-index",
+                owner=self.admin,
+            )
+        )
+        self.index.save_revision().publish()
+
+        locale = self.index.locale
+        self.category = Category.objects.create(
+            name="Agriculture",
+            slug="agriculture",
+            locale=locale,
+        )
+        self.other_category = Category.objects.create(
+            name="Environment",
+            slug="environment",
+            locale=locale,
+        )
+        self.tag = Tag.objects.create(name="News", slug="news")
+        self.other_tag = Tag.objects.create(name="Report", slug="report")
+        self.organization = Organization.objects.create(name="INRAE", slug="inrae")
+        self.other_organization = Organization.objects.create(name="ANSES", slug="anses")
+        self.author = Person.objects.create(
+            name="Jane Doe",
+            role="Writer",
+            organization=self.organization,
+        )
+        self.other_author = Person.objects.create(
+            name="John Smith",
+            role="Editor",
+            organization=self.other_organization,
+        )
+
+        self.post_with_category = self._create_post(
+            "Post Agriculture",
+            blog_categories=[self.category],
+        )
+        self.post_with_other_category = self._create_post(
+            "Post Environment",
+            blog_categories=[self.other_category],
+        )
+        self.post_with_tag = self._create_post("Post News", tags=[self.tag])
+        self.post_with_other_tag = self._create_post("Post Report", tags=[self.other_tag])
+        self.post_with_author = self._create_post("Post Jane", authors=[self.author])
+        self.post_with_other_author = self._create_post("Post John", authors=[self.other_author])
+
+    def _create_post(self, title, blog_categories=None, tags=None, authors=None):
+        post = BlogEntryPage(
+            title=title,
+            date=datetime(2024, 1, 1, 12, 0, 0, tzinfo=self.paris_tz),
+            owner=self.admin,
+        )
+        self.index.add_child(instance=post)
+        for category in blog_categories or []:
+            post.blog_categories.add(category)
+        for tag in tags or []:
+            post.tags.add(tag)
+        for author in authors or []:
+            post.authors.add(author)
+        post.save_revision().publish()
+        return post
+
+    def _set_filter_settings(self, **settings):
+        for field, value in settings.items():
+            setattr(self.index, field, value)
+        self.index.save_revision().publish()
+
+
+class BlogIndexPageFilterVisibilityTest(BlogIndexPageFilterTestBase):
+    def test_filter_shown_when_enabled(self):
+        for case in FILTER_CASES:
+            with self.subTest(case["name"]):
+                self._set_filter_settings(**{case["setting"]: True})
+                response = self.client.get(self.index.url)
+                self.assertContains(response, case["heading"])
+                self.assertContains(response, case["visible_label"](self))
+
+    def test_filter_hidden_when_disabled(self):
+        for case in FILTER_CASES:
+            with self.subTest(case["name"]):
+                self._set_filter_settings(**{case["setting"]: False})
+                response = self.client.get(self.index.url)
+                self.assertNotContains(response, case["heading"])
+
+
+class BlogIndexPageFilterQueryTest(BlogIndexPageFilterTestBase):
+    def test_filters_posts(self):
+        for case in FILTER_CASES:
+            with self.subTest(case["name"]):
+                response = self.client.get(case["filter_url"](self))
+                self.assertContains(response, case["matching_title"](self))
+                self.assertNotContains(response, case["other_title"](self))
+
+    def test_url_filter_applies_even_when_filter_disabled_in_settings(self):
+        """
+        Disabling a filter hides its sidemenu block, but get_context still
+        filters posts from the query param.
+        """
+        for case in FILTER_CASES:
+            with self.subTest(case["name"]):
+                self._set_filter_settings(**{case["setting"]: False})
+                response = self.client.get(case["filter_url"](self))
+                self.assertNotContains(response, case["heading"])
+                self.assertContains(response, case["matching_title"](self))
+                self.assertNotContains(response, case["other_title"](self))
+
+    def test_filters_posts_with_two_query_params(self):
+        # Remove the "source" case, because there's interactions with the "author" case that make testing complicated.
+        # We'll have less coverage but reliable tests.
+        filter_cases = [case for case in FILTER_CASES if case["name"] != "source"]
+        for case_a, case_b in combinations(filter_cases, 2):
+            with self.subTest(f"{case_a['name']}+{case_b['name']}"):
+                title = f"Post with {case_a['name']} and {case_b['name']}"
+                kwargs = {**case_a["post_kwargs"](self), **case_b["post_kwargs"](self)}
+                matching = self._create_post(title, **kwargs)
+                query = f"{self.index.url}?" f"{case_a['query_param'](self)}&{case_b['query_param'](self)}"
+                response = self.client.get(query)
+                self.assertContains(response, matching.title)
+                # Check that posts with only one filter do not show.
+                for case in (case_a, case_b):
+                    self.assertNotContains(response, case["matching_title"](self))
+                    self.assertNotContains(response, case["other_title"](self))
+
+
+class BlogIndexPagePostsDisplayTest(BlogIndexPageFilterTestBase):
+    def test_posts_display_categories_on_cards(self):
+        post = self._create_post(
+            "Post with category",
+            blog_categories=[self.category],
+        )
+        response = self.client.get(self.index.url)  # no filters
+        category_tag = f'<p class="fr-tag">{self.category.name}</p>'
+        soup = BeautifulSoup(response.content, "html.parser")
+        matching_card = None
+        for card in soup.select("div.fr-card"):
+            tag_html = "".join(str(tag) for tag in card.select("p.fr-tag"))
+            if post.title in card.get_text() and category_tag in tag_html:
+                matching_card = card
+                break
+        self.assertIsNotNone(
+            matching_card,
+            "Expected a post card containing the title and the category tag.",
+        )
